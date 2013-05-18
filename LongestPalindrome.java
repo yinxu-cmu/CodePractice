39d8b23a43a66f9fd6dc3053b5c3dbf6fa4b0f74
yinxu-cmu <remonx@gmail.com>
2013-05-18 00:37:46 -0300

    Update LongestPalindrome.java

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,28 +1,53 @@
 /**
- *find the longest panlindrome in a string: abeeddddeabe
+ *find the longest panlindrome in a string
+ * extremely slow edition.....
  */
 import java.util.*;
 
 public class LongestPalindrome{
 
-	public static void main(String[] args) {
-		String input = "abeeddddeabe";
-		int len = input.length();
-		int pos = 0;
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < len/2; i++) {
-			if(input.charAt(i) == input.charAt(len-1-i)){
-				String pair = Character.toString(input.charAt(i)) 
-						+ Character.toString(input.charAt(i));
-				//System.out.println(pair);
-				sb.insert(pos, pair);
-				pos++;
-			}else{
-				sb.setLength(0);
-				pos = 0;
-			}
-			
-		}
-		System.out.println(sb);
-	}
+	
+	
+	public String longestPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        char[] cArray = s.toCharArray();
+        int max = 0;
+        String palin = "";
+        if(cArray.length == 1){
+            return s;
+        }
+        for(int i = 0;i < cArray.length - 1; i++){
+            /* i as center */
+            String palinT = Character.toString(cArray[i]);
+            
+                for(int j = 1; i-j >= 0 && i+j < cArray.length; j++){
+                    if(cArray[i-j] == cArray[i+j]){
+                        palinT = cArray[i-j] + palinT + cArray[i+j];
+                    }else{
+                        break;
+                    }
+                    
+                }
+            if(palinT.length() >= palin.length()){
+                    palin = palinT;
+                }
+                
+            if(cArray[i] == cArray[i+1]){
+                palinT = Character.toString(cArray[i]) + Character.toString(cArray[i+1]);
+                for(int l = 1; i-l >= 0 && i+l+1 < cArray.length;l++){
+                    if(cArray[i-l] == cArray[i+l+1]){
+                        palinT = cArray[i-l] + palinT + cArray[i+1+l];
+                    }else{
+                       break;
+                    }
+                }
+                 if(palinT.length() >= palin.length()){
+                            palin = palinT;
+                    }
+            }
+            
+        }
+        return palin;
+    }
 }
