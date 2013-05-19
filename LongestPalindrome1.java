d5984d3ecb31fd1d922112ee71ebdf9387ac542d
yinxu-cmu <remonx@gmail.com>
2013-05-18 22:46:54 -0300

    Update LongestPalindrome1.java

--- LongestPalindrome1.java
+++ LongestPalindrome1.java
@@ -1,26 +1,59 @@
-/**
- *find the longest panlindrome in a string: abeeddddeabe
- */
-import java.util.*;
-
-public class LongestPalindrome1{
-
+public class LongestPalindrome {
+    
 	public static void main(String[] args) {
-		String input = "abeeddddeabe";
-		int len = input.length();
-		String palin = "";
-		for (int i = 0; i < len/2; i++) {
-			if(input.charAt(i) == input.charAt(len-1-i)){
-				palin += input.charAt(i);
-			}else{
-				palin = "";
-			}
-		}
-		System.out.println(palin);
-		int len1 = palin.length();
-		for (int j = len1 - 1; j >= 0; j--) {
-			palin += palin.charAt(j);
-		}
-		System.out.println(palin);
+		String input = "baba";
+		String output = longestPalindrome(input);
+		System.out.println(output);
 	}
+    public static String longestPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int len = s.length();
+        String palin = "";
+        int maxlen = 0;
+        int start = 0;
+        int end = 0;
+        /* length <= 1 */
+        if(len <= 1){
+            return s;
+        }
+        
+        for(int i=0; i < len-1; i++){
+            /* i as center */
+            start = i;
+            end = i;
+            while(start >= 0 && end < len){
+                if(s.charAt(start) == s.charAt(end)){
+                    start--;
+                    end++;
+                }else{
+                    break;
+                }
+            }
+            start++;
+            //end--;
+            if(end - start + 1> maxlen){
+                maxlen = end - start + 1;
+                palin = s.substring(start, end);
+            }
+            /* i, i+1 as center */
+            start = i;
+            end = i+1;
+            while(start >= 0 && end < len){
+                if(s.charAt(start) == s.charAt(end)){
+                    start--;
+                    end++;
+                }else{
+                    break;
+                }
+            }
+            start++;
+            //end--;
+            if(end - start + 1 > maxlen){
+                maxlen = end - start + 1;
+                palin = s.substring(start, end);
+            }
+        }
+        return palin;
+    }
 }
