6f888ca4b0eee03252a4db0cf83e0ef90b6e6931
yinxu-cmu <remonx@gmail.com>
2013-05-22 09:07:01 -0300

    Update and rename reverseInt to reverseInt.java

--- reverseInt.java
+++ reverseInt.java
@@ -2,6 +2,12 @@ public class Solution {
     public int reverse(int x) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        
+        /* 1. overflow after revers
+         * 2. special case: 10, 100
+         * 3. how to detect overflow
+         */
+         
         int res = 0;
         int tmp = x;
         int result = 0;
